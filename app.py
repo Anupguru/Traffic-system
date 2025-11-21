770de25637e153983010cd26038383114cd7a27e
Anupguru <anup52398@gmail.com>
2025-11-21 11:11:58 +0600

    app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ st.markdown("""
 # ------------------------------------
 # LOAD MODEL
 # ------------------------------------
-MODEL_PATH = "/home/heru/lungten/traffic_duration_model.keras"
+MODEL_PATH = "traffic_duration_model.keras"
 
 try:
     model = tf.keras.models.load_model(MODEL_PATH, compile=False)
